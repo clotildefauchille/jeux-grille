08ed30136352dd7e197f045e0dfdea435c179102
clotildefauchille <clotildefauchille@gmail.com>
2020-10-27 23:26:57 +0100

    ne marche pas

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
 var app = {
   init: function () {
     console.log('init !');
+    app.board = document.querySelector("#board");
     app.player = {
       x: 0,
       y: 0,
       direction: "right",
-    }
+    };
     app.targetCell = {
       x: 5,
       y: 3,
-    }
-    app.drawBoard()
+    };
+    app.drawBoard();
+
   },
   drawBoard: function () {
     var ROWS = 4;
@@ -18,22 +20,30 @@ var app = {
     for (let index = 0; index < ROWS; index++) {
       var row = document.createElement("div");
       row.className = "row";
-      var board = document.querySelector("#board");
-      board.appendChild(row);
+      app.board.appendChild(row);
+      var rowSelect = document.querySelectorAll(".cell");
+      // // var currentCell = cells[0];
+      if (rowSelect[index] = rowSelect[app.targetCell.x] && (rowSelect[index] = rowSelect[app.targetCell.y])) {
+        cellule.classList.add("targetCell");
+      } else if (rowSelect[index] = rowSelect[app.player.x] && (rowSelect[index] = rowSelect[app.player.y])) {
+        cellule.classList.add("player");
+      }
       for (var index2 = 0; index2 < COLUMNS; index2++) {
         var cellule = document.createElement('div');
         cellule.className = "cell";
         row.appendChild(cellule);
-        var currentCell = document.querySelectorAll(".cell");
-        if (currentCell[index] = app.targetCell.x && (currentCell[index] = app.targetCell.y)) {
-          cellule.classList.add("targetCell");
-        } else if (currentCell[index] = app.player.x && (currentCell[index] = app.player.y)) {
-          cellule.classList.add("player");
-        }
+        
       }
     }
 
 
+  },
+  clearBoard: function () {
+    app.board.textContent = ""
+  },
+  redrawBoard: function () {
+    app.clearBoard();
+    app.drawBoard();
   }
 
 
