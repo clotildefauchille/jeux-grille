2747aea8370b9e2dd8741b8ff47fc3899452eacb
clotildefauchille <clotildefauchille@gmail.com>
2020-11-11 22:02:48 +0100

    case joueur case arrivée + reset mais probleme 3 div player

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var app = {
   init: function () {
-    console.log('init !');
+
     app.board = document.querySelector("#board");
     app.player = {
       x: 0,
@@ -15,27 +15,29 @@ var app = {
 
   },
   drawBoard: function () {
-    var ROWS = 4;
-    var COLUMNS = 6
+    var ROWS = app.targetCell.y + 1;
+    var COLUMNS = app.targetCell.x + 1;
     for (let index = 0; index < ROWS; index++) {
       var row = document.createElement("div");
       row.className = "row";
       app.board.appendChild(row);
-      var rowSelect = document.querySelectorAll(".cell");
-      // // var currentCell = cells[0];
-      if (rowSelect[index] = rowSelect[app.targetCell.x] && (rowSelect[index] = rowSelect[app.targetCell.y])) {
-        cellule.classList.add("targetCell");
-      } else if (rowSelect[index] = rowSelect[app.player.x] && (rowSelect[index] = rowSelect[app.player.y])) {
-        cellule.classList.add("player");
-      }
-      for (var index2 = 0; index2 < COLUMNS; index2++) {
+
+      for (let index2 = 0; index2 < COLUMNS; index2++) {
         var cellule = document.createElement('div');
-        cellule.className = "cell";
+        let currentCell = cellule.className = "cell";
         row.appendChild(cellule);
-        
+      }
+      let cells = document.querySelectorAll(".cell");
+      let laCase = cells[index];
+      if (laCase = cells[(app.targetCell.y + 1) * (app.targetCell.x + 1) - 1]) {
+        laCase.classList.add("targetCell");
+      }
+      else if (laCase = cells[(app.player.y + 1) * (app.player.x + 1) - 1]){
+        let divPlayer=document.createElement("div");
+        divPlayer.classList.add("player");
+        laCase.appendChild(divPlayer);
       }
     }
-
 
   },
   clearBoard: function () {
@@ -44,7 +46,8 @@ var app = {
   redrawBoard: function () {
     app.clearBoard();
     app.drawBoard();
-  }
+  },
+  
 
 
 };
