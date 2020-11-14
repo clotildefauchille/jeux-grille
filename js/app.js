757a2f593d899c239d4df7520a6cfe715215f406
clotildefauchille <clotildefauchille@gmail.com>
2020-11-14 22:34:44 +0100

    gerer rotation ok

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 var app = {
   init: function () {
-
     app.board = document.querySelector("#board");
     app.player = {
       x: 0,
@@ -11,10 +10,12 @@ var app = {
       x: 5,
       y: 3,
     };
-    app.drawBoard();
 
+    app.directionUp = app.player.direction[2];
+    app.directionDown = app.player.direction[3];
+    app.drawBoard(app.directionDown);
   },
-  drawBoard: function () {
+  drawBoard: function (direction) {
     var ROWS = app.targetCell.y + 1;
     var COLUMNS = app.targetCell.x + 1;
     for (let index = 0; index < ROWS; index++) {
@@ -23,40 +24,53 @@ var app = {
       app.board.appendChild(row);
 
       for (let index2 = 0; index2 < COLUMNS; index2++) {
-        var cellule = document.createElement('div');
-        let currentCell = cellule.className = "cell";
+        var cellule = document.createElement("div");
+        cellule.className = "cell";
         row.appendChild(cellule);
       }
       let cells = document.querySelectorAll(".cell");
       let laCase = cells[index];
-      if (laCase = cells[(app.targetCell.y + 1) * (app.targetCell.x + 1) - 1]) {
+      if (
+        (laCase = cells[(app.targetCell.y + 1) * (app.targetCell.x + 1) - 1])
+      ) {
         laCase.classList.add("targetCell");
-      } else if (laCase = cells[(app.player.y + 1) * (app.player.x + 1) - 1]) {
-        let divPlayer = document.createElement("div");
-        divPlayer.classList.add("player");
-        laCase.appendChild(divPlayer);
+      } else if (
+        (laCase = cells[(app.player.y + 1) * (app.player.x + 1) - 1])
+      ) {
+        app.divPlayer = document.createElement("div");
+        app.divPlayer.classList.add("player");
+        laCase.appendChild(app.divPlayer);
+
+        if (direction === app.directionUp) {
+          app.divPlayer.classList.add("player-up");
+        } else if (direction === app.directionDown) {
+          app.divPlayer.classList.add("player-down");
+        }
       }
     }
-
   },
   clearBoard: function () {
-    app.board.textContent = ""
+    app.board.textContent = "";
   },
-  redrawBoard: function () {
+  redrawBoard: function (direction) {
     app.clearBoard();
-    app.drawBoard();
+    app.drawBoard(direction);
   },
-  turnLeft: () => {
+  turnLeft: (direction) => {
     app.player.direction[1];
     app.player.x--;
-    app.redrawBoard();
+
+    app.redrawBoard(direction);
   },
-  turnRight: () => {
+  turnRight: (direction) => {
     app.player.direction[0];
     app.player.x++;
+    app.redrawBoard(direction);
+  },
+  moveFoward: () => {
+    y++;
     app.redrawBoard();
-  }
-
+  },
 };
 
-document.addEventListener('DOMContentLoaded', app.init);
+document.addEventListener("DOMContentLoaded", app.init);
